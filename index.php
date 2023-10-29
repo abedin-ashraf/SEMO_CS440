14f38b9ada7cf354e87379dd3fc196e41f065787
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2023-10-29 11:00:44 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+    <link rel="stylesheet" href="css/s1.css">
+</head>
+<body>
+    <div id="full">
+        <div id="inner_full">
+            <div id="header">
+                <h2>Redhawks Blood Bank Management System</h2>
+            </div>
+            <div id="body">
+                <br><br><br><br><br>
+                <table>
+                    <tr>
+                        <td width="100px" height="50px"><b>Enter Username</b></td>
+                        <td width="100px" height="50px"><input type="text" name="un" placeholder= "Enter Username" ></td>
+                        
+                        <td width="100px" height="50px"><b>Enter Password</b></td>
+                        <td width="100px" height="50px"><input type="text" name="ps" placeholder= "Enter Password" ></td>
+                    </tr>
+                </table>
+            </div>
+            <div id="footer">Footer</div>
+        </div>
+    </div>
+</body>
+</html>
