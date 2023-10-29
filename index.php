72c29ec30060e9b0df2dfb9c85fbb8d6260bc9b9
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2023-10-29 12:08:33 -0500

    added index.html

--- index.php
+++ index.php
@@ -14,17 +14,21 @@
             </div>
             <div id="body">
                 <br><br><br><br><br>
-                <table>
+                <table align="center">
                     <tr>
-                        <td width="100px" height="50px"><b>Enter Username</b></td>
-                        <td width="100px" height="50px"><input type="text" name="un" placeholder= "Enter Username" ></td>
-                        
-                        <td width="100px" height="50px"><b>Enter Password</b></td>
-                        <td width="100px" height="50px"><input type="text" name="ps" placeholder= "Enter Password" ></td>
+                        <td width="150px" height="50px"><b>Enter Username</b></td>
+                        <td width="150px" height="50px"><input type="text" name="un" placeholder= "Enter Username" style="width: 180px; height:30px; border-radius: 10px; text-align: center;"></td>
+                    </tr>
+                    <tr>    
+                        <td width="150px" height="50px"><b>Enter Password</b></td>
+                        <td width="150px" height="50px"><input type="text" name="ps" placeholder= "Enter Password" style="width: 180px; height:30px; border-radius: 10px; text-align: center;"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" name="sub" value="Login" style="width:70px; height:30px; border-radius: 5px"></td>
                     </tr>
                 </table>
             </div>
-            <div id="footer">Footer</div>
+            <div id="footer"><h4 align="center">Copyright@mohammedashraf</h4></div>
         </div>
     </div>
 </body>
